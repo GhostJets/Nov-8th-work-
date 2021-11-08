d43d426168dcf4dcc14fbc61c669192762f52d1a
Benjamin <82902690+Vented1@users.noreply.github.com>
2021-11-08 12:24:59 -0500

    q3
    q3

---
+++ q3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+void adressprint () {
+  cout<<("Lasalle Ontario");
+}void nameprint () {
+  cout<<("Ben Ferderbar");
+}int main () {
+cout<<"**************************\n";
+adressprint();
+cout<<"\n";
+nameprint () ;
+cout<<"\n*************************";
+}
